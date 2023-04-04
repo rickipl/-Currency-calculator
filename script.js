223c5faf53fe713a90d8b19196b9a462a182a787
rickipl <57848674+rickipl@users.noreply.github.com>
2023-04-05 00:43:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
-kurs_euro();
-kurs_funta();
-kurs_dolara();                          //wywołanie funkcji pobierających kursy walut
-kurs_franka();
-
 new Vue({
 
     data(){
 
         return{
+         
+            loading: true,
             quantity: null,
             waluta: "Wybierz",
             counter: 0,
-            euro: 'Pobierz kurs',
-            funt: 'Pobierz kurs',
-            dolar: 'Pobierz kurs',
-            frank: 'Pobierz kurs',      //zmienne VUE
+            euro: 0,
+            funt: 0,
+            dolar: 0,
+            frank: 0,
+            dataLoaded: false,     //zmienne VUE
             warunek: "false",
             finish: null,
             end: '',
@@ -22,65 +20,71 @@ new Vue({
 
         };
     },
+    async mounted() {
 
+        await this.getData();
+        this.loading = false;
+
+      },
     methods:{
 
-        kurs(){
-            
-            kurs_euro();
-            this.euro = eur;
-            kurs_funta();
-            this.funt = fun;            //funkcja ustawiająca kursy walut 
-            kurs_dolara();
-            this.dolar = dol;
-            kurs_franka();
-            this.frank = fra;
-
-        },
+        async getData() {
+            const responseEur = await fetch('https://api.nbp.pl/api/exchangerates/rates/A/eur/');
+            const dataEur = await responseEur.json();                                                   //pobranie kursu waluty Euro
+            this.euro = dataEur.rates[0].mid;
+      
+            const responseGbp = await fetch('https://api.nbp.pl/api/exchangerates/rates/A/gbp/');
+            const dataGbp = await responseGbp.json();                                                   //pobranie kursu waluty Funt Brytyjski
+            this.funt = dataGbp.rates[0].mid;
+      
+            const responseUsd = await fetch('https://api.nbp.pl/api/exchangerates/rates/A/usd/');
+            const dataUsd = await responseUsd.json();                                                   //pobranie kursu waluty Dolar Amerykański
+            this.dolar= dataUsd.rates[0].mid;
+      
+            const responseChf = await fetch('https://api.nbp.pl/api/exchangerates/rates/A/chf/');
+            const dataChf = await responseChf.json();                                                   //pobranie kursu waluty Frank szwajcarski
+            this.frank = dataChf.rates[0].mid;
+          },
 
         euro_(){
             this.end = "";
-            kurs_euro();
-            this.euro = eur;
             this.waluta = "PLN na Euro";
             this.pln = "Euro na PLN";
             this.counter = this.euro;       //funkcja waluty Euro
             this.warunek = "true";
             this.quantity = 1;
             this.end_wal = "Euro";
+            this.finish = null;
         },
         funt_brytyjski(){
             this.end = "";
-            kurs_funta();
-            this.funt = fun;
             this.waluta = "PLN na Funty";
             this.pln = "Funty na PLN";
             this.counter = this.funt;       //funkcja waluty Funt Brytyjski
             this.warunek = "true";
             this.quantity = 1;
             this.end_wal = "Funta";
+            this.finish = null;
         },
         dolar_amerykanski(){
             this.end = "";
-            kurs_dolara();
-            this.dolar = dol;
             this.waluta = "PLN na Dolary";
             this.pln = "Dolary na PLN";
             this.counter = this.dolar;      //funkcja waluty Dolar Amerykański
             this.warunek = "true";
             this.quantity = 1;
             this.end_wal = "Dolara";
+            this.finish = null;
         },
         frank_szwajcarski(){
             this.end = "";
-            kurs_franka();
-            this.frank = fra;
             this.waluta = "PLN na Franki";
             this.pln = "Franki na PLN";
             this.counter = this.frank;      //funkcja waluty Frank szwajcarski
             this.warunek = "true";
             this.quantity = 1;
             this.end_wal = "Franka";
+            this.finish = null;
         },
         currency(){
             if(this.quantity <= 0){
@@ -90,10 +94,10 @@ new Vue({
             }                                                   //użytkownika nie jest ujemna
             else
             {
-            this.quantity = this.quantity * this.counter;
+            this.finish = this.quantity * this.counter;
             this.end = 'zł';                                    //przeliczanie wybranej waluty na złotówki
-            this.quantity = this.quantity.toFixed(2);
-            this.finish = this.quantity;
+            this.finish = this.finish.toFixed(2);
+            
             }
         },
         fcurrency(){
@@ -104,10 +108,10 @@ new Vue({
             }                                                   //użytkownika nie jest ujemna
             else
             {
-            this.quantity = this.quantity / this.counter;
+            this.finish = this.quantity / this.counter;
             this.end = this.end_wal;                            //przeliczanie waluty z złotówek na inną walutę 
-            this.quantity = this.quantity.toFixed(2);
-            this.finish = this.quantity;
+            this.finish = this.finish.toFixed(2);
+            
             }
         },
     },
@@ -117,54 +121,3 @@ let eur;
 let fun;
 let dol;                                                        //zmienne JS
 let fra;
-
-function kurs_euro(){
-
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function() {
-    if(ajax.readyState === 4 && ajax.status === 200) {
-        let res = JSON.parse(this.responseText);                //pobranie kursu waluty Euro
-        eur = res.rates[0].mid;
-    }
-};
-ajax.open("GET", "http://api.nbp.pl/api/exchangerates/rates/a/eur/", true);
-ajax.send();
-}
-
-function kurs_funta(){
-
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function() {
-    if(ajax.readyState === 4 && ajax.status === 200) {
-        let res = JSON.parse(this.responseText);                //pobranie kursu waluty Funt Brytyjski
-        fun = res.rates[0].mid;
-    }
-};
-ajax.open("GET", "http://api.nbp.pl/api/exchangerates/rates/a/gbp/", true);
-ajax.send();
-}
-function kurs_dolara(){
-
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function() {
-    if(ajax.readyState === 4 && ajax.status === 200) {          //pobranie kursu waluty Dolar Amerykański
-        let res = JSON.parse(this.responseText);
-        dol = res.rates[0].mid;
-    }
-};
-ajax.open("GET", "http://api.nbp.pl/api/exchangerates/rates/a/usd/", true);
-ajax.send();
-}
-
-function kurs_franka(){
-
-var ajax = new XMLHttpRequest();
-ajax.onreadystatechange = function() {
-    if(ajax.readyState === 4 && ajax.status === 200) {          //pobranie kursu waluty Frank szwajcarski
-        let res = JSON.parse(this.responseText);
-        fra = res.rates[0].mid;
-    }
-};
-ajax.open("GET", "http://api.nbp.pl/api/exchangerates/rates/a/chf/", true);
-ajax.send();
-}
